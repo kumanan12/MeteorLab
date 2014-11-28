0ef3914188c5e021f01ba09255a3d277d92c2732
kumanan12 <san11kav>
2014-11-28 15:52:05 -0500

    Initial commit.

---
+++ simple-todos.js
@@ -0,0 +1,13 @@
+Tasks = new Mongo.Collection("tasks");
+if (Meteor.isClient) {
+// This code only runs on the client
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+
+
+
+}
